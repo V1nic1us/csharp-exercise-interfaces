f6a8bc95e22fca7c042c9c847c38dd86f1f5509f
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-23 00:27:21 -0400

    feat: creation of the PaypalService class to calculate the rate applied each month

---
+++ csharp-exercise-interfaces/Services/PaypalService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercise_interfaces.Services
+{
+    internal class PaypalService : IPaymentService
+    {
+        public double PaymentFee(double amount)
+        {
+            return amount * 0.02;
+        }
+
+        public double Interest(double amount, int months)
+        {
+            return amount * 0.01 * months;
+        }
+    }
+}
