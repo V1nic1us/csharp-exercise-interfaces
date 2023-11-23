f22a9c4110fdb6c63b58241fa9569dc2edfd13d1
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-23 00:21:31 -0400

    feat: creation of the installment and method class to display the date and value

---
+++ csharp-exercise-interfaces/Entities/Installment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp_exercise_interfaces.Services;
+
+namespace csharp_exercise_interfaces.Entities
+{
+    internal class Installment: IInstallment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+        public void PayInstallment()
+        {
+            Console.WriteLine($"Installment due date: {DueDate.ToString("dd/MM/yyyy")}");
+            Console.WriteLine($"Installment amount: {Amount.ToString("F2")}");
+        }
+    }
+}
