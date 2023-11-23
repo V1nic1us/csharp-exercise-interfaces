b8ff623bb79f0115ae51320d718d7482320650d9
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-23 00:19:40 -0400

    feat: added codes to register contracts and generate installments

--- csharp-exercise-interfaces/Program.cs
+++ csharp-exercise-interfaces/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using csharp_exercise_interfaces.Entities;
+using csharp_exercise_interfaces.Services;
+
+namespace csharp_exercise_interfaces
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            var number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            var date = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract value: ");
+            var value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            var installments = int.Parse(Console.ReadLine());
+
+            var contract = new Contract(number, date, value);
+            var contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(contract, installments);
+
+            Console.WriteLine();
+            Console.WriteLine("Installments:");
+            foreach (var installment in contract.Installments)
+            {
+                installment.PayInstallment();
+            }
+        }
+    }
+}
