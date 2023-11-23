7ae3d8b7119ca0141604ddc694e1e715f1542deb
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-23 00:23:11 -0400

    feat: creation of contract service method to calculate the value of the installment

---
+++ csharp-exercise-interfaces/Services/ContractService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp_exercise_interfaces.Entities;
+
+namespace csharp_exercise_interfaces.Services
+{
+    internal class ContractService : IContractService
+    {
+        private readonly IPaymentService _paymentService;
+
+        public ContractService(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        public void ProcessContract(Contract contract, int installments)
+        {
+            var installmentValue = contract.Value / installments;
+            for (int i = 1; i <= installments; i++)
+            {
+                var installmentDate = contract.Date.AddMonths(i);
+                var interest = _paymentService.Interest(installmentValue, i);
+                var paymentFee = _paymentService.PaymentFee(installmentValue + interest);
+                var installment = new Installment(installmentDate, installmentValue + interest + paymentFee);
+                contract.Installments.Add(installment);
+            }
+        }
+    }
+}
