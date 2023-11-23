886350e58d149594e7cae2e91e811e39865d6d07
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-23 00:20:31 -0400

    feat: creation of the contract and method class to save the installments

---
+++ csharp-exercise-interfaces/Entities/Contract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp_exercise_interfaces.Services;
+
+namespace csharp_exercise_interfaces.Entities
+{
+    internal class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double Value { get; set; }
+        public List<IInstallment> Installments { get; set; }
+
+        public Contract(int number, DateTime date, double value)
+        {
+            Number = number;
+            Date = date;
+            Value = value;
+            Installments = new List<IInstallment>();
+        }
+
+        public void AddInstallment(IInstallment installment)
+        {
+            Installments.Add(installment);
+        }
+    }
+}
